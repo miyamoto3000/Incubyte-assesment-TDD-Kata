676ec12d12477b58135737b0d673db661432f40b
miyamoto3000 <shahidmulani0045@gmail.com>
2025-12-12 22:18:20 +0530

    first commit

---
+++ TDD-Kata/src/main/java/com/TDD/Kata/TddKataApplication.java
@@ -0,0 +1,13 @@
+package com.TDD.Kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddKataApplication.class, args);
+	}
+
+}
---
+++ TDD-Kata/src/test/java/com/TDD/Kata/TddKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TDD.Kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
